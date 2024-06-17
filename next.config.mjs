9212b189b8a6a707f9cb69af28dc497ac966e661
mohamad alabudi <mohamadalabudi42@gmail.com>
2024-06-16 22:11:56 -0500

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withPwa from 'next-pwa';
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true, // Enable React strict mode for improved error handling
+    swcMinify: true,      // Enable SWC minification for improved performance
+    compiler: {
+        removeConsole: process.env.NODE_ENV !== "development", // Remove console.log in production
+    },
+};
+
+// Configuration object tells the next-pwa plugin
+const pwaConfig = {
+    dest: "public", // Destination directory for the PWA files
+    disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
+    register: true, // Register the PWA service worker
+    skipWaiting: true, // Skip waiting for service worker activation
+};
+
+// Export the combined configuration for Next.js with PWA support
+export default withPwa(pwaConfig)(nextConfig);
