1b41ab6e7361794fdc4934a939b186ed377a3258
mohamad alabudi <mohamadalabudi42@gmail.com>
2024-06-16 22:46:45 -0500

    Added blank pages and folder hierarchy

---
+++ src/app/capture/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Capture() {
+    return (
+        <>
+            <h1>Capture My Drive</h1>
+            <Link href='/'>Go Back</Link>
+        </>
+    );
+}
---
+++ src/app/maps/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Maps() {
+    return (
+        <>
+            <h1>Maps</h1>
+            <Link href='/'>Go Back</Link>
+        </>
+    );
+}
