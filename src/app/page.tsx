777e820f303f1c2b9efbb0d7850fb18eb863b8eb
moe <msalma@umich.edu>
2024-09-15 18:30:10 -0400

    implemented main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,16 @@ import styles from './page.module.css';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1>Welcome to Deer Tracker!</h1>
-      <h3>Powered by AI & Machine Learning</h3>
+    <main className={styles.mainpageContainer}>
+      <div className={styles.headerconatiner}>
+        <header className={styles.h1}>Welcome to Deer Tracker!</header>
+      </div>
+      <div className={styles.lgnbttncontainer}>
+        <button className={styles.loginbtton}>login</button>
+      </div>
+      <div className={styles.signbttncontainer}>
+        <button className={styles.signupbtton}>sign up</button>
+      </div>
       <BottomNav />
     </main>
   );
