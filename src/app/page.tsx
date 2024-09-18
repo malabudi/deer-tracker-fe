32f30cc6820be14b69b0446749fa26c0b700139d
moe <msalma@umich.edu>
2024-09-18 14:19:04 -0400

    Updated Main Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
           onClick={() => console.log('su Button clicked!')}
         />
       </div>
-      <BottomNav />
+      {/*<BottomNav />*/}
     </main>
   );
 }
