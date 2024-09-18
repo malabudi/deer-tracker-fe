3ad5e28d80f3b3c7c890462d5a95d67f74cdd351
Mohamad Alabudi <mohamadalabudi42@gmail.com>
2024-09-18 17:56:22 -0400

    added mutation hook to camera page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import React from 'react';
+import { ReactQueryProvider } from './react-query-provider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -23,12 +24,14 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ReactQueryProvider>{children}</ReactQueryProvider>
+      </body>
     </html>
   );
 }
