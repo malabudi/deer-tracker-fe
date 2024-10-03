032206a93c189140b5b25e0b642fb9cb16a65be8
Heshoo-ksh <hishamodeh2001@gmail.com>
2024-10-03 18:53:46 -0400

    PR review update

--- src/app/maps/page.tsx
+++ src/app/maps/page.tsx
@@ -4,25 +4,34 @@ import React, { useEffect } from 'react';
 import Map from '@/components/map/Map';
 import getLocation from '@/hooks/useLocation';
 import BottomNav from '@/components/bottom-nav/BottomNav';
-import useGetSightingsByLocation from '@/hooks/useGetSightingsByLocation';
+import getSightingsByLocation from '@/hooks/getSightingsByLocation';
 
 const Maps: React.FC = () => {
   const {
     latitude: currentLatitude,
     longitude: currentLongitude,
-    //callCount,
+    // callCount,
   } = getLocation({ onLocationUpdate: () => {} });
 
-  const apiKey = ' '; // (grab form discord) a story has been created to find a better approach
+  const apiKey = ' '; // (grab from discord) a story has been created to find a better approach
 
-  // Hook for fetching deer sightings
-  const { getSightingsByLocation } = useGetSightingsByLocation();
-
-  // Testing fetching sightings with given coordinates (longitude, latitude, radius)
+  // Fetch sightings using useEffect for testing purposes
   useEffect(() => {
-    // For testing purposes, calling the function with fixed coordinates.
-    getSightingsByLocation(-73.985428, 40.748817, 1); //Sould CONSOLE.LOG two locations, same response as this POSTMAN URL: http://localhost:5000/api/get_sightings_by_locationAndRadius?longitude=-73.985428&latitude=40.748817&radius=1
-  }, [getSightingsByLocation]);
+    const fetchSightings = async () => {
+      try {
+        const sightings = await getSightingsByLocation(
+          -73.985428,
+          40.748817,
+          1
+        );
+        console.log('Fetched sightings:', sightings);
+      } catch (error) {
+        console.error('Error fetching sightings:', error);
+      }
+    };
+
+    fetchSightings();
+  }, []);
 
   return (
     <>
@@ -31,23 +40,6 @@ const Maps: React.FC = () => {
         longitude={currentLongitude}
         apiKey={apiKey}
       />
-      {/* DEBUG
-      <div>
-        <h2>Current Location:</h2>
-        <p>
-          Latitude: {currentLatitude !== null ? currentLatitude : 'Fetching...'}
-        </p>
-        <p>
-          Longitude:{' '}
-          {currentLongitude !== null ? currentLongitude : 'Fetching...'}
-        </p>
-        {process.env.NODE_ENV === 'development' && (
-          <div>
-            <h2>API Call Count: {callCount}</h2>
-          </div>
-        )}
-      </div>
-      */}
       <BottomNav />
     </>
   );
