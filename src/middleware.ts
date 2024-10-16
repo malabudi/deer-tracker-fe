ed2b9c11eebb545c0fb036b680956d42a600664d
Mohamad Alabudi <mohamadalabudi42@gmail.com>
2024-10-16 16:35:16 -0400

    fixed bug where if authenticated and visiting root page, it does not redirect

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,22 @@
 // Without a defined matcher, this line applies next-auth to the entire project
 export { default } from 'next-auth/middleware';
+import { getToken } from 'next-auth/jwt';
+import { NextRequest, NextResponse } from 'next/server';
 
-// Pages that the user cannot visit unless they login
+export async function middleware(req: NextRequest) {
+  const token = await getToken({ req });
+  const { pathname } = req.nextUrl;
+  const isAuthenticated = !!token;
+
+  // if user is authenticated and visits the root page, redirect to /settings
+  if (pathname === '/' && isAuthenticated) {
+    return NextResponse.redirect(new URL('/settings', req.url));
+  }
+
+  return NextResponse.next();
+}
+
+// Pages that the user cannot visit unless they login (protected pages)
 export const config = {
   matcher: ['/', '/capture', '/maps', '/settings'],
 };
