ee8fec584a7dd9090d866b5ea891d3c86d29f7c7
Mohamad Alabudi <80290328+malabudi@users.noreply.github.com>
2024-10-25 21:13:30 -0400

    Import deer model (#28)
    * imported deer model, must fix bug with highlighting, prediction json not properly read
    
    * imported deer model, must fix bug with highlighting, prediction json not properly read
    
    * imported the deer model from roboflow API
    
    * removed logs from debugging
    
    * removed comments in settings page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,9 @@
 import Link from 'next/link';
-import BottomNav from '@/components/bottom-nav/BottomNav';
 import './globals.css';
 
 export default function NotFound() {
   return (
     <>
-      <BottomNav />
       <div className="not-found-container">
         <h1>404</h1>
         <h2>Page Not Found</h2>
--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -4,17 +4,21 @@ import BottomNav from '@/components/bottom-nav/BottomNav';
 import styles from './page.module.css';
 import ActiveButton from '@/components/Active-Button/ActiveButton';
 import RadioButton from '@/components/Radio-Button/RadioButton';
-import ToggleSwitch from '@/components/Toggle-Button/ToggleButton';
+import { signOut } from 'next-auth/react';
+
 export default function Settings() {
   const [selectedTheme, setSelectedTheme] = useState('');
-  const [isToggled, setIsToggled] = useState(false);
 
   const handleThemeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSelectedTheme(event.target.value);
   };
-  const handleToggle = () => {
-    setIsToggled(!isToggled);
+
+  const handleSignOut = () => {
+    signOut({
+      callbackUrl: '/', // Redirect to homepage after sign out
+    });
   };
+
   return (
     <>
       <div className={styles.settingpageContainer}>
@@ -25,7 +29,7 @@ export default function Settings() {
             <ActiveButton text="Edit Account" />
           </div>
           <div className={styles.LogoutContainer}>
-            <ActiveButton text="Log out" />
+            <ActiveButton text="Log out" onClick={handleSignOut} />
           </div>
           <hr className={styles.lineSpliter}></hr>
           <h2 className={styles.SectionLabel}>Theme</h2>
@@ -45,9 +49,6 @@ export default function Settings() {
               onChange={handleThemeChange}
             />
           </div>
-          <hr className={styles.lineSpliter}></hr>
-          <h2 className={styles.SectionLabel}>Notifications</h2>
-          <ToggleSwitch isOn={isToggled} handleToggle={handleToggle} />
         </div>
       </div>
       <BottomNav />
