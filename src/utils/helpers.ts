fd5726eda201608d93f89435dfdb6b63c48a02b5
Mohamad Alabudi <mohamadalabudi42@gmail.com>
2024-11-02 01:40:47 -0400

    fixed bug with radio button not prechecking current theme

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -6,3 +6,15 @@ export const canParseJson = (input: string) => {
     return false; // If an error occurs, return false
   }
 };
+
+// Helper functions to get and set cookies
+export const getCookie = (name) => {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(';').shift();
+};
+
+export const setCookie = (name, value, days) => {
+  const expires = new Date(Date.now() + days * 864e5).toUTCString();
+  document.cookie = `${name}=${value}; expires=${expires}; path=/`;
+};
