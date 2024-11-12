c32baf8c8a700257a48047ea5d03eabc221e509d
Mohamad Alabudi <mohamadalabudi42@gmail.com>
2024-11-12 17:21:09 -0500

    removed NODE_ENV

--- next.config.mjs
+++ next.config.mjs
@@ -5,11 +5,9 @@ const nextConfig = {
   reactStrictMode: true, // Enable React strict mode for improved error handling
   swcMinify: true, // Enable SWC minification for improved performance
   compiler: {
-    removeConsole:
-      process.env.NODE_ENV === 'beta' || process.env.NODE_ENV === 'prod', // Remove console.log in production
+    removeConsole: false, // Remove console.log in production
   },
   env: {
-    NODE_ENV: process.env.NODE_ENV,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     RESEND_API_KEY: process.env.RESEND_API_KEY,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
@@ -21,7 +19,7 @@ const nextConfig = {
 // Configuration object tells the next-pwa plugin
 const pwaConfig = {
   dest: 'public', // Destination directory for the PWA files
-  disable: process.env.NODE_ENV !== 'beta' || process.env.NODE_ENV !== 'prod', // Disable PWA in development mode
+  disable: true, // Disable PWA in development mode
   register: true, // Register the PWA service worker
   skipWaiting: true, // Skip waiting for service worker activation
 };
