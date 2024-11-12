2443abb41997f3e30dd98ea95ba1afa2a22509aa
Mohamad Alabudi <mohamadalabudi42@gmail.com>
2024-11-12 16:58:53 -0500

    disabled PWA

--- next.config.mjs
+++ next.config.mjs
@@ -19,7 +19,7 @@ const nextConfig = {
 // Configuration object tells the next-pwa plugin
 const pwaConfig = {
   dest: 'public', // Destination directory for the PWA files
-  disable: process.env.NODE_ENV === 'development', // Disable PWA in development mode
+  disable: true, // Disable PWA in development mode
   register: true, // Register the PWA service worker
   skipWaiting: true, // Skip waiting for service worker activation
 };
